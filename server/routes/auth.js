0339481f40c58aa52a211ecb2b8437de6600c59e
Aman Singh <aman.iit96@gmail.com>
2022-10-19 00:50:04 +0530

    login route created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const authRouter = Router();
 
-authRouter.post("/signup", (req, res) => {
+authRouter.post("/signup", async (req, res) => {
   const { name, username, password } = req.body;
+
+  const isUsernameExist = await User.findOne({ username });
+
+  if (isUsernameExist) {
+    return res.send({ message: "Username already exist" });
+  }
+
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
   const user = new User({
@@ -18,6 +26,23 @@ authRouter.post("/signup", (req, res) => {
     }
     return res.status(201).send({ message: "Signup Success" });
   });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.send(401).send("Invalid Credentials");
+  }
+  const token = jwt.sign(
+    {
+      name: user.name,
+      userid: user._id,
+    },
+    "SECRET",
+    { expiresIn: "4 hour" }
+  );
+  return res.send({ message: "login Succes", token: token });
 });
 
 module.exports = authRouter;
