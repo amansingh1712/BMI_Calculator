794d491f9c5905bb1b2aedbfb06d231c0a3a7bf0
Aman Singh <aman.iit96@gmail.com>
2022-10-19 00:26:06 +0530

    hashing of password is done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,23 +1,21 @@
 const { Router } = require("express");
-
 const User = require("../models/User");
-
+const bcrypt = require("bcryptjs");
 const authRouter = Router();
 
 authRouter.post("/signup", (req, res) => {
   const { name, username, password } = req.body;
-
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
   const user = new User({
     name,
     username,
-    password,
+    password: hash,
   });
-
   user.save((err, success) => {
     if (err) {
       return res.status(500).send({ message: "Error Occured while signup" });
     }
-
     return res.status(201).send({ message: "Signup Success" });
   });
 });
